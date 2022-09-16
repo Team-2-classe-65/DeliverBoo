f08d1b90b4d28fd0c6d776446fed5612c3c7f64b
Debby95 <debbyjacksonstar@hotmail.it>
2022-09-16 11:01:57 +0200

    routes created

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,9 +3,41 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class OrderController extends Controller
 {
-    //
+    public function index() 
+    {
+        $orders= Order::all();
+
+        return view("admin.orders.index", compact("orders"));
+    }
+
+    public function edit($id)
+    {
+        $order = Order::findOrFail($id);
+        return view("admin.orders.edit", compact("order"));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $order = Order::findOrFail($id);
+
+        if(!$order->details){
+            $order->details = new OrderDetail();
+            $order->details->order_id = $order->id;
+            $order->details->fill($data);
+            $order->details->save();
+        } else {
+            $order->details->update($data);
+        }
+
+        $order->update($data);
+        return redirect()->route("admin.orders.edit", $order->id);
+    }
 }
