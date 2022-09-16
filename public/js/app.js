73e28773d79c8e43289f32fdd0f3580e13217bf6
Debby95 <debbyjacksonstar@hotmail.it>
2022-09-16 11:17:42 +0200

    agffd

--- public/js/app.js
+++ public/js/app.js
@@ -40245,13 +40245,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
-__webpack_require__(/*! C:\Users\Hybri\Documents\Boolean\Esercizi\Deliverboo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Hybri\Documents\Boolean\Esercizi\Deliverboo\resources\sass\app.scss */"./resources/sass/app.scss");
-=======
-__webpack_require__(/*! C:\Users\annat\Documents\Deliverboo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\annat\Documents\Deliverboo\resources\sass\app.scss */"./resources/sass/app.scss");
->>>>>>> features/Anna-issues
+__webpack_require__(/*! C:\Users\utente\Boolean\Deliverboo\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\utente\Boolean\Deliverboo\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
