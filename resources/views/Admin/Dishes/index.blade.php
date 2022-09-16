a6caae30b3ef40e368bac48859262cb857e418c8
paolopu91 <paolo.pugliese1991@gmail.com>
2022-09-16 11:41:51 +0200

    changed view

--- resources/views/Admin/Dishes/index.blade.php
+++ resources/views/Admin/Dishes/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.home')
+@extends('layouts.app')
 
 @section('content')
     <div class="container">
@@ -6,10 +6,10 @@
             <div class="col-12">
                 <h1>Tutti i posts</h1>
                 <div class="d-flex justify-content-between align-items-center mb-2">
-                    <a href="{{ route('admin.posts.create') }}" class="btn btn-success">
+                    <a href="{{ route('admin.dishes.create') }}" class="btn btn-success">
                         Crea nuovo post
                     </a>
-                    <a href="{{ route('admin.index') }}"><i class="fa-solid fa-3x fa-circle-arrow-left btn-back"></i></a>
+                    <a href="{{ route('admin.home') }}"><i class="fa-solid fa-3x fa-circle-arrow-left btn-back"></i></a>
                 </div>
                 <table class="table">
                     <thead>
@@ -17,31 +17,22 @@
                             <th>Titolo</th>
                             <th>Slug</th>
                             <th>Autore</th>
-                            <th>Tags</th>
                             <th>Categoria</th>
                             <th class="text-center">Azioni</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($posts as $post)
+                        @foreach ($dishes as $dish)
                             <tr>
-                                <td>{{ $post->title }}</td>
-                                <td>{{ $post->slug }}</td>
-                                <td>{{ $post->user ? $post->user->name : '' }}</td>
+                                <td>{{ $dish->title }}</td>
+                                <td>{{ $dish->slug }}</td>
+                                <td>{{ $dish->user ? $dish->user->name : '' }}</td>
                                 <td>
-                                    @foreach ($post->tags as $tag)
-                                        <a href="{{ route('admin.tags.show', $tag->id) }}" class="text-decoration-none">{{ $tag->name }}</a>
-                                        @if (!$loop->last)
-                                            <span> -</span>
-                                        @endif
-                                    @endforeach
-                                </td>
-                                <td>
-                                    <div>{{ $post->category ? $post->category->name : '' }}</div>
+                                    <div>{{ $dish->category ? $dish->category->name : '' }}</div>
                                 </td>
                                 <td class="text-center">
                                     <a class="btn btn-info btn-sm"
-                                        href="{{ route('admin.posts.show', ['post' => $post->slug]) }}">
+                                        href="{{ route('admin.dish.show', ['post' => $dish->slug]) }}">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                             stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
@@ -50,7 +41,7 @@
                                         </svg>
                                     </a>
                                     <a class="btn btn-warning btn-sm"
-                                        href="{{ route('admin.posts.edit', ['post' => $post->slug]) }}">
+                                        href="{{ route('admin.dish.edit', ['post' => $dish->slug]) }}">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                             stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
@@ -59,7 +50,7 @@
                                         </svg>
                                     </a>
                                     <form class="d-inline-block"
-                                        action="{{ route('admin.posts.destroy', ['post' => $post->slug]) }}" method="post">
+                                        action="{{ route('admin.dish.destroy', ['post' => $dish->slug]) }}" method="post">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="btn btn-danger btn-sm">
