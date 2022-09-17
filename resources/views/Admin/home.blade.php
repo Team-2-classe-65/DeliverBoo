db2db9af4d73b8a2c0fd2c2fd99e67a55832d2d5
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-09-17 12:57:08 +0200

    bo

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -4,13 +4,23 @@
     <div class="container p-3 h-100">
         <h1 class="mb-5">
             Benvenuto <strong>{{ Auth::user()->name }}</strong>
+
+            <span class="ms-5">
+                @foreach (Auth::user()->categories as $category)
+                    {{ $category->name }}
+                    @if (!$loop->last)
+                        <span class="mx-1">-</span>
+                    @endif
+                @endforeach
+            </span>
+
         </h1>
 
         <div class="row h-75">
             <div class="col">
                 <a href="{{ route('admin.dishes.index') }}" class="text-uppercase">
                     <div class="card position-realtive h-100 x overflow-hidden">
-                        <img src="{{asset('img/dishes.jpg')}}" alt="" class="w-100 rounded">
+                        <img src="{{ asset('img/dishes.jpg') }}" alt="" class="w-100 rounded">
                         <h2>
                             Piatti
                         </h2>
@@ -29,6 +39,6 @@
 
         </div>
 
-        
+
     </div>
 @endsection
