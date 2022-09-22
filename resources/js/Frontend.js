d54ecc82247bedcdf9fc0dde36205a0313f77b9f
paolopu91 <paolo.pugliese1991@gmail.com>
2022-09-22 17:37:24 +0200

    aggiunto bootstrap in appjs e bs toggle

--- resources/js/Frontend.js
+++ resources/js/Frontend.js
@@ -2,12 +2,9 @@ import Vue from "vue";
 import App from "./views/App.vue";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+require('./bootstrap');
 
 AOS.init();
-<<<<<<< HEAD
-=======
-
->>>>>>> feature/add-aos-library
 
 new Vue({
     el:'#app',
