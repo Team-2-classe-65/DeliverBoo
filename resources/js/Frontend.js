1c100499e87e41125436321f6a5ae2d0b26f56d3
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-09-22 18:27:57 +0200

    update header

--- resources/js/Frontend.js
+++ resources/js/Frontend.js
@@ -6,14 +6,11 @@ import 'aos/dist/aos.css';
 require('./bootstrap');
 
 AOS.init();
-<<<<<<< HEAD
 
 import VueRouter from "vue-router";
 import {routes} from "./routes";
 
 Vue.use(VueRouter);
-=======
->>>>>>> 92618fd7c58166adf9c3ee35c51affe553388661
 
 new Vue({
     el:'#app',
