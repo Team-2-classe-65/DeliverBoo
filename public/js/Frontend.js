9fb99174fddcfb366e25e7e88c77b9d732152f4f
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-09-25 01:04:51 +0200

    repair jumbotron

--- public/js/Frontend.js
+++ public/js/Frontend.js
@@ -46058,7 +46058,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\utente\Boolean\Deliverboo\resources\js\frontend.js */"./resources/js/frontend.js");
+module.exports = __webpack_require__(/*! C:\Users\Hybri\Documents\Boolean\Esercizi\Deliverboo\resources\js\frontend.js */"./resources/js/frontend.js");
 
 
 /***/ })
